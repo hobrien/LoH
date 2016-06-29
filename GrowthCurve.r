b8f839fb2ad34611faa8cbeb96bb9b96668e1b4a
Heath O'Brien <heath.obrien@gmail.com>
2016-06-29 18:34:04 +0100

    used purr to nest models in df

--- GrowthCurve.r
+++ GrowthCurve.r
@@ -2,6 +2,8 @@ library(ggplot2)
 library(nlsMicrobio)
 library(tidyr)
 library(dplyr)
+library(purrr)
+library(broom)
 prepareData <- function(df, temp) {
   df2 <- gather(df, "t", "OD", 4:ncol(df)) 
   df2$Temperature <- as.factor(temp)
@@ -11,35 +13,37 @@ prepareData <- function(df, temp) {
 df <- read.delim("~/BTSync/Code/LoH/growth.txt", header=TRUE, check.names = FALSE)
 
 df<-prepareData(df, 50)
+mid_time <- ifelse(length(df$t) %% 2 == 0, median(df$t[1:length(df$t)-1]), median(df$t))
+mid_od <-log10(median(df$OD))
 ggplot(df, aes(x=t, y=log10(OD), group=paste(Sample, Temperature), colour=Temperature)) +
   geom_point(size=.01) +
+  geom_text(aes(label=round(mumax, digits=3)), data=labels, colour='black', size=2, y=min(log10(df$OD)), vjust=-1, hjust=1) +
   facet_grid(Row ~ Column) +
   theme(axis.text=element_blank()) +
   theme(axis.ticks=element_blank()) +
   theme(legend.position="bottom") +
   theme(legend.direction= "horizontal")
 
-MuMax <- function(od, time){
-    test = data.frame(OD=od, t=time)
-    #test <-subset(df, Sample == name)
+df2 <- group_by(df, Sample) %>% nest()
+
+MuMax <- function(test){
     test$LOG10N<-log10(test$OD)
     test <- select(test, t, LOG10N)
-    if (max(test$LOG10N)-min(test$LOG10N) > 0.1) {
-         mumax<- tryCatch({
-          nls1 <- nls(baranyi, test, list(lag=1, mumax=min(1,(max(test$LOG10N)-min(test$LOG10N))*2), LOG10N0 = min(test$LOG10N), LOG10Nmax = max(test$LOG10N)))
-          nls1$m$getPars()[2][[1]]
-        }, error = function(err) {
-          NA
-        })
-        
-        #plot(test)
-        #plotfit(nls1, smooth=TRUE)
-        
-    }
-    else {
-        mumax <- 0
-    }
-    mumax
+      nls(baranyi, test, list(lag=1, 
+                            mumax=min(1,(max(test$LOG10N)-min(test$LOG10N))*2), 
+                            LOG10N0 = min(test$LOG10N), 
+                            LOG10Nmax = max(test$LOG10N)))
 }
-
-AllMuMax <-df %>% group_by(Sample) %>% summarise(mumax=MuMax(OD, t))
+AllMuMax <- mutate(df2, Mod=map(data, possibly(MuMax, NA)))
+AllMuMax <- mutate(df2, Mod=map(data, MuMax))
+AllMuMax <- mutate(AllMuMax, params= Mod %>% map(tidy))
+labels <- unnest(AllMuMax, params) %>% 
+  subset(term == 'mumax') %>% 
+  select(Sample, mumax=round(estimate, digits=2), mumax.se=std.error) %>%
+  full_join(df)
+  
+labels <- unnest(AllMuMax, params) %>% 
+  subset(term == 'LOG10Nmax') %>% 
+  select(Sample, nmax=estimate, nmax.se=std.error) %>%
+  full_join(labels) %>% 
+  filter(t==max(df$t))
